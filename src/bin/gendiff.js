745b426f9bc650853f13ba6e4ea59245f7c52b73
mikhail.a <mkhlslkhnv@gmail.com>
2019-04-16 00:38:22 +1000

    fix syntax

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,7 +5,7 @@ import { version } from '../../package.json';
 const program = commander;
 
 program
-  .version('version')
+  .version(version)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
