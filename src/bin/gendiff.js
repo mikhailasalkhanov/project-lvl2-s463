a08ec456d62eeb3ebc6204eedaa9eb542382ca42
mikhail.a <mkhlslkhnv@gmail.com>
2019-04-24 19:30:59 +1000

    add genDiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 import commander from 'commander';
 import { version } from '../../package.json';
+import { readFileSync } from 'fs';
+import genDiff from '../';
 
 const program = commander;
 
@@ -9,4 +11,5 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
+  .action((file1, file2) => console.log(genDiff(file1, file2)))
   .parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { has, union } from 'lodash';
+import { readFileSync } from 'fs';
+
+const genDiff = (beforeFilePath, afterFilePath) => {
+  const before = JSON.parse(readFileSync(beforeFilePath, 'UTF-8'));
+  const after = JSON.parse(readFileSync(afterFilePath, 'UTF-8'));
+  const allKeys = union(Object.keys(before), Object.keys(after));
+
+  const result = allKeys.reduce((acc, key) => {
+    if (has(before, key) && has(after, key) && before[key] === after[key]) {
+      return [...acc, `    ${key}: ${before[key]}`];
+    }
+    if (has(before, key) && !has(after, key)) {
+      return [...acc, `  - ${key}: ${before[key]}`];
+    }
+    if (!has(before, key) && has(after, key)) {
+      return [...acc, `  + ${key}: ${after[key]}`];
+    }
+    return [...acc, `  + ${key}: ${after[key]}`, `  - ${key}: ${before[key]}`];
+  }, []);
+  
+  return ['{', ...result, '}'].join('\n');
+};
+
+export default genDiff;
